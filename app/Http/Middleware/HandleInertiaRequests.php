7cd57a170ef1d16a050cffadca5ab1e9e227c362
Okafor Obinna <kingsloob1@gmail.com>
2025-10-17 05:55:12 +0100

    add a logger

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -35,6 +35,7 @@ class HandleInertiaRequests extends Middleware
      */
     public function share(Request $request): array
     {
+        request()->session()->put('info', 'Kindly update your biography to continue.')
         dd($request->session()->get('info'));
         return [
             ...parent::share($request),
