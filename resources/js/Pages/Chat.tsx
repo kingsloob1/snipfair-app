f4748ca757efdfe1acd52fbbff4bb9cb49cdcc40
Okafor Obinna <kingsloob1@gmail.com>
2025-10-23 03:23:39 +0100

    fix bug with chat component

--- resources/js/Pages/Chat.tsx
+++ resources/js/Pages/Chat.tsx
@@ -36,7 +36,7 @@ const Chat = ({
     } else if (auth.user.role === 'stylist') {
         return (
             <StylistAuthLayout fullScreen={true} header="Stylist Chat">
-                <section className="mt-6 min-h-screen w-full max-w-7xl px-2 md:px-5">
+                <section className="mx-auto max-w-7xl flex-1 px-1.5 py-2 sm:px-5 sm:py-6 md:py-8">
                     <ChatsComponent
                         conversations={conversations}
                         initialSelectedConversationId={selectedConversationId}
